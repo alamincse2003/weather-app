717d3ae9d2d9a4a768d78b399fa110d38f29507c
alamincse2003 <mdalamincse2003@gmail.com>
2024-11-15 20:33:32 +0600

    weather app initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Weather from "./Components/Weather/Weather";
+
+
+const App = () => {
+  return (
+    <div className="app">
+      <Weather></Weather>
+    </div>
+  );
+};
+
+export default App;
