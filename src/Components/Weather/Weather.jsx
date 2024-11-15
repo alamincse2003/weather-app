e6b556f1e6e3c55fb334a078f2e3ae320f49c873
alamincse2003 <mdalamincse2003@gmail.com>
2024-11-15 21:15:23 +0600

    weather app UI design completed

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,40 +1,54 @@
-import './Weather.css';
-import search_icon from '../../assets/search.png';
-import clear_icon from '../../assets/clear.png';
+import "./Weather.css";
+import search_icon from "../../assets/search.png";
+import clear_icon from "../../assets/clear.png";
 // import cloud_icon from '../../assets/cloud.png';
 // import drizzle_icon from '../../assets/drizzle.png';
-import humidity_icon from '../../assets/humidity.png';
+import humidity_icon from "../../assets/humidity.png";
 // import rain_icon from '../../assets/rain.png';
 // import snow_icon from '../../assets/snow.png';
-import wind_icon from '../../assets/wind.png';
+import wind_icon from "../../assets/wind.png";
+import { useEffect } from "react";
 const Weather = () => {
-    return (
-      <div className="weather">
-        <div className="search-bar">
-          <input type="text" placeholder="Search" />
-          <img src={search_icon} alt="" />
-        </div>
-        <img src={clear_icon} alt="" className="weather-icon" />
-        <p className="temperature">16°C</p>
-        <p className="location">Location</p>
-        <div className="weather-data">
-          <div className="col">
-            <img src={humidity_icon} alt="" />
-            <div>
-              <p>91 %</p>
-              <span>Humidity</span>
-            </div>
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+    };
+    useEffect(() => {
+        search("London"); // Example city to fetch weather data for. Replace with desired city.
+    },[])
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">16°C</p>
+      <p className="location">Location</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
           </div>
-          <div className="col">
-            <img src={wind_icon} alt="" />
-            <div>
-              <p>3.6 Km/h</p>
-              <span>Wind Speed</span>
-            </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6 Km/h</p>
+            <span>Wind Speed</span>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Weather;
